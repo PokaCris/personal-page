84462fe7a23151407a8eeb50c0510ff9d12dd829
PokaCris <after_the_rain999@mail.ru>
2025-06-03 22:30:35 +0300

    ang

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InfoAboutComponent } from './info-about/info-about.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [InfoAboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/info-about/info-about.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-about',
+  imports: [],
+  templateUrl: './info-about.component.html',
+  styleUrl: './info-about.component.css'
+})
+export class InfoAboutComponent {
+
+}
