ba6f11da43be2167b9892bcd1c2ab94f589ee0b3
PokaCris <after_the_rain999@mail.ru>
2025-06-08 00:54:24 +0300

    ang2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { InfoAboutComponent } from './info-about/info-about.component';
+import { HeaderComponent } from './header/header.component';
+import { InfoComponent } from './info/info.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { KnowledgeComponent } from './knowledge/knowledge.component';
 
 @Component({
   selector: 'app-root',
-  imports: [InfoAboutComponent],
+  imports: [ 
+    HeaderComponent, 
+    InfoComponent,
+    ExperienceComponent,
+    KnowledgeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
